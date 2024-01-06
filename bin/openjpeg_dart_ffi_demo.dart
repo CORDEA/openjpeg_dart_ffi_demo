07696d6b1f6040899422f707dd14331df9c9c363
Yoshihiro Tanaka <contact@cordea.jp>
2024-01-06 12:01:39 +0900

    initial commit :)

---
+++ bin/openjpeg_dart_ffi_demo.dart
@@ -0,0 +1,5 @@
+import 'package:openjpeg_dart_ffi_demo/openjpeg_dart_ffi_demo.dart' as openjpeg_dart_ffi_demo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${openjpeg_dart_ffi_demo.calculate()}!');
+}
---
+++ test/openjpeg_dart_ffi_demo_test.dart
@@ -0,0 +1,8 @@
+import 'package:openjpeg_dart_ffi_demo/openjpeg_dart_ffi_demo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
